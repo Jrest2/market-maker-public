90c4768bc70eb96b955861accf203e0052b81a57
Andrii <andreygrt2@gmail.com>
2024-06-02 10:36:22 +0400

    Added logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { InxModule } from './inx/inx.module';
-import { ConfigModule } from '@nestjs/config';
-import { HttpModule } from '@nestjs/axios';
+import { LoggingInterceptor } from './logger/logger.interceptor';
+import { WinstonModule } from 'nest-winston';
+import { loggerConfig } from './logger/logger.config';
 
 @Module({
   imports: [
@@ -12,8 +16,15 @@ import { HttpModule } from '@nestjs/axios';
       isGlobal: true,
     }),
     HttpModule,
+    WinstonModule.forRoot(loggerConfig),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+  ],
 })
 export class AppModule {}
---
+++ src/logger/logger.config.ts
@@ -0,0 +1,15 @@
+import * as winston from 'winston';
+import 'winston-daily-rotate-file';
+
+export const loggerConfig = {
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.DailyRotateFile({
+      filename: 'logs/application-%DATE%.log',
+      datePattern: 'YYYY-MM-DD',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
+    }),
+  ],
+};
